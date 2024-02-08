9f49daa19b2f682703191b6534e546b180d66e30
Leo-Brd <leobordet.pro@gmail.com>
2024-02-08 14:57:12 +0100

    exo js

---
+++ Exercice.js
@@ -0,0 +1,16 @@
+let age = 25;
+const name = 'Alice';
+
+console.log(age*2);
+console.log((age/3)*2);
+
+if (age >= 18){
+    console.log("C'est un adulte");
+}else{
+    console.log("C'est un mineur");
+}
+
+while(age != 0){
+    console.log(age);
+    age -= 1;
+}
