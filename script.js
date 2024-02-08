9d00aed4ef2b873d1cb1fedd9507a72d38e65f6e
Leo-Brd <leobordet.pro@gmail.com>
2024-02-08 15:10:38 +0100

    btn form

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-/*
+
 document.addEventListener('DOMContentLoaded', function() {
-    alert('La page est complètement chargée !');     
+    // alert('La page est complètement chargée !');  
+    const boutonModifier = document.getElementById("submit");
+    const Form = document.getElementById("form");   
+
+    boutonModifier.addEventListener("click", function(event) {
+        event.preventDefault();
+        Form.style.display = "none";
+      });
 });
-*/
 
 
