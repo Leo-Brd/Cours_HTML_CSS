8c07fdbf4199ec1085324b4372a1fa4d9f34ac24
Leo-Brd <leobordet.pro@gmail.com>
2024-02-09 10:08:57 +0100

    delete js

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+// Exercice Test
+let age = 25;
+const name = 'Alice';
+
+console.log(age*2);
+console.log((age/3)*2);
+
+if (age >= 18){
+    console.log("C'est un adulte");
+}else{
+    console.log("C'est un mineur");
+}
+
+while(age != 0){
+    console.log(age);
+    age -= 1;
+}
+
+
+// Exos Fondamentaux
+
+
